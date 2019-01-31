138a211fe4345a9e6a3dc7b01a0f8e2c64ddff4e
NeelimaYenugula-UHCL <neelima.y28@gmail.com>
2019-01-30 23:06:34 -0600

    Update logicalOperators.js

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -9,6 +9,8 @@ logicalOperatorsAnswers = {
    * @returns {bool} True if either a, b, or both are true, false otherwise.
    */
   or: function or(a, b) {
+    
+    return a || b;
 
   },
 
@@ -22,6 +24,6 @@ logicalOperatorsAnswers = {
    * @returns {bool} True if either a and b are both true, false otherwise.
    */
   and: function and(a, b) {
-
+    return a&&b;
   }
 };
