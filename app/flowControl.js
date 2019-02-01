3f4ea21fa869b31b29d64a7e76b7973a4d6294f6
NeelimaYenugula-UHCL <neelima.y28@gmail.com>
2019-01-31 22:16:05 -0600

    Update flowControl.js

--- app/flowControl.js
+++ app/flowControl.js
@@ -16,11 +16,11 @@ flowControlAnswers = {
     {
       if(num % 3 === 0)
         return 'fizz';
-      else if(num % 5 === 0)
+      if(num % 5 === 0)
         return 'buzz';
-      else if(num % 3 === 0 && num % 5 === 0)
+      if(num % 3 === 0 && num % 5 === 0)
         return 'fizzbuzz';
-      else
+      
         return num;
     }
     else
